d802fdab87311df784c9abe31404f07234104f72
SAMBERENO <arek4@spoko.pl>
2026-05-05 22:13:34 +0200

    finished createTransitLetter()

---
+++ src/main/java/com/example/listmanagmentapp/helpmethods/FillMissingFromPrevious.java
@@ -0,0 +1,11 @@
+package com.example.listmanagmentapp.helpmethods;
+
+public class FillMissingFromPrevious {
+
+    public FillMissingFromPrevious() {}
+
+    public void fillMissingFromPrevious() {
+
+    }
+
+}
--- src/main/java/com/example/listmanagmentapp/service/ListCreatingService.java
+++ src/main/java/com/example/listmanagmentapp/service/ListCreatingService.java
@@ -70,25 +70,37 @@ public class ListCreatingService {
             Sheet sheet = workbook.getSheetAt(0);
 
             int row = 0;
+            int neededSize = fromDBtoDto().size();
+            int sumaUszczelek = 0;
+            int sumaBrakow = 0;
+            int sumaNiezgodnosci = 0;
+            String pudla = null;
             for(int i = 17; i < 30; i++) {
-                if(row >= fromDBtoDto().size()){
+                if(row >= neededSize){
                     break;
                 }
-                sheet.getRow(i).getCell(1).setCellValue(fromDBtoDto().get(row).nrWyrobu());
-                sheet.getRow(i).getCell(2).setCellValue(fromDBtoDto().get(row).nrZlecenia());
-                sheet.getRow(i).getCell(3).setCellValue(fromDBtoDto().get(row).sumaUszczelek());
-                sheet.getRow(i).getCell(4).setCellValue(fromDBtoDto().get(row).sumaBrakow());
-                for(CodesWithValues e : fromDBtoDto().get(row).braki().getMap().values()) {
-                    if (e.value() != 0) {
-                        String damagedResults = new StringBuilder().append(e.value()).append(e.letter()).toString();
-                        sheet.getRow(i).getCell(7).setCellValue(damagedResults);
+                if(fromDBtoDto().get(row).nrWyrobu().matches(".*//.*")) {
+                    i--;
+                    pudla = new StringBuilder().append(",").append(pudla).toString();
+                }
+                else {
+                    pudla = "";
+                    sumaUszczelek = 0;
+                    sumaBrakow = 0;
+                    sumaNiezgodnosci = 0;
+                    sheet.getRow(i).getCell(1).setCellValue(fromDBtoDto().get(row).nrWyrobu());
+                    sheet.getRow(i).getCell(2).setCellValue(fromDBtoDto().get(row).nrZlecenia());
+                }
+                //TODO: Możliwe że będzie zmniejszyć zakres w StringBuilder().delete(0, 3) na .delete(0, 2)
+                    pudla = new StringBuilder().append(fromDBtoDto().get(row).pudla()).delete(0, 3).append(pudla).toString();
+                    sheet.getRow(i).getCell(3).setCellValue(sumaUszczelek += fromDBtoDto().get(row).sumaUszczelek());
+                    sheet.getRow(i).getCell(4).setCellValue(sumaBrakow += fromDBtoDto().get(row).sumaBrakow());
+                    sheet.getRow(i).getCell(8).setCellValue(sumaNiezgodnosci += fromDBtoDto().get(row).niezgodnosci());
+                    sheet.getRow(i).getCell(7).setCellValue(pudla);
+                    if (fromDBtoDto().get(row).kz()){
+                        sheet.getRow(i).getCell(9).setCellValue("KZ");
                     }
-                }
-                sheet.getRow(i).getCell(8).setCellValue(fromDBtoDto().get(row).niezgodnosci());
-                if (fromDBtoDto().get(row).kz()){
-                    sheet.getRow(i).getCell(9).setCellValue("KZ");
-                }
-                row++;
+                    row++;
             }
 
             workbook.write(fout);
@@ -106,15 +118,31 @@ public class ListCreatingService {
     }
 
 
-
+//TODO:Na następny raz to do zrobienia :3
     public void createShortagesLetter(){
         try(FileInputStream fin = new FileInputStream(inputPath + "FormatkaRuchyBrakow.xlsx");
             XSSFWorkbook workbook = new XSSFWorkbook(fin);
             FileOutputStream fout = new FileOutputStream(outputPath + "Braki " + date + ".xlsx")){
 
-
-
-
+            Sheet sheet = workbook.getSheetAt(1);
+/*
+            int row = 0;
+            for(int i = 17; i < 30; i++) {
+                if(row >= fromDBtoDto().size()){
+                    break;
+                }
+                for(CodesWithValues e : fromDBtoDto().get(row).braki().getMap().values()) {
+                    if (e.value() != 0) {
+                        String damagedResults = new StringBuilder().append(e.value()).append(e.letter()).toString();
+                        sheet.getRow(i).getCell(7).setCellValue(damagedResults);
+                    }
+                }
+                if (fromDBtoDto().get(row).kz()){
+                    sheet.getRow(i).getCell(9).setCellValue("KZ");
+                }
+                row++;
+            }
+*/
             workbook.write(fout);
         } catch (FileNotFoundException e) {
             System.out.println("Nie znaleziono pliku RuchyBrakow: " + e.getMessage());
