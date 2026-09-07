07275bedd39136f1461a3277c0b68f80bccf6dbf
SAMBERENO <arek4@spoko.pl>
2026-09-07 20:37:14 +0200

    Before deployment.

--- src/main/java/com/example/listmanagmentapp/ListManagementApp.java
+++ src/main/java/com/example/listmanagmentapp/ListManagementApp.java
@@ -1,6 +1,5 @@
 package com.example.listmanagmentapp;
 
-import com.example.listmanagmentapp.service.BarcodeReaderService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,19 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ListManagementApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(ListManagementApp.class, args);
-
-        BarcodeReaderService barcodeReaderService = new BarcodeReaderService();
-
-        barcodeReaderService.scanBarCodes("C:\\Users\\arek4\\OneDrive\\Pulpit(1)\\kodyTest2.png");
-
-        //ImagePreProcessing imagePreProcessing = new ImagePreProcessing();
-
-        //imagePreProcessing.saveImage("C:\\Users\\arek4\\OneDrive\\Pulpit(1)\\pobrane.jpg");
-
-        //ImagePreProcessingDeWarping imagePreProcessingDeWarping = new ImagePreProcessingDeWarping();
-
-        //imagePreProcessingDeWarping.saveImage("C:\\Users\\arek4\\OneDrive\\Pulpit(1)\\pobrane1.jpg");
-    }
+        SpringApplication.run(ListManagementApp.class, args);}
 
 }
