c18b69641808203598b86b8292d732927f85de29
SAMBERENO <arek4@spoko.pl>
2026-09-09 15:28:31 +0200

    Modified .gitignore

--- src/main/java/com/example/listmanagmentapp/ListManagementApp.java
+++ src/main/java/com/example/listmanagmentapp/ListManagementApp.java
@@ -1,15 +1,11 @@
 package com.example.listmanagmentapp;
 
-import com.example.listmanagmentapp.config.ConfigurationFileReader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.FileNotFoundException;
-import java.io.IOException;
 
 @SpringBootApplication
 public class ListManagementApp {
 
-    public static void main(String[] args) throws IOException {SpringApplication.run(ListManagementApp.class, args);}
+        static void main(String[] args) {SpringApplication.run(ListManagementApp.class, args);}
 
 }
